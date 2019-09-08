2f35a16349125e5090e12432cdf45d5e5d658577
Sim Yong Seng <simyseng@gmail.com>
2019-09-08 14:10:21 +0700

    Textfield added

--- User Entry/ViewController.swift
+++ User Entry/ViewController.swift
@@ -9,7 +9,13 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var textField: UITextField!
+    
+    @IBAction func runButtonTapped(_ sender: Any) {
+        if let text = textField.text {
+            print(text)
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
